f18ce452b1d29794d9471cb3ecd7c227e5a8f619
cw <fromnowwon@gmail.com>
2024-07-20 01:16:00 +0900

    feat: api 요청 함수 생성

---
+++ src/util/api.ts
@@ -0,0 +1,8 @@
+const BASE_URL = import.meta.env.VITE_API_URL;
+
+export const fetchCharacters = async (page: number) => {
+  const response = await fetch(`${BASE_URL}/character?page=${page}`);
+  const data = response.json();
+
+  return data;
+};
