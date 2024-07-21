c6d68b6d0de1d07b7f979357f7994a799f700139
cw <fromnowwon@gmail.com>
2024-07-21 22:10:49 +0900

    feat: pagination 로직 분리, 스타일 추가

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,23 +1,44 @@
+import { CgArrowLeft, CgArrowRight } from "react-icons/cg";
 import { useCharacterStore } from "../store/useCharacterStore";
 
 export default function Pagination() {
   const { page, setPage } = useCharacterStore();
 
+  const handlePrevious = () => {
+    if (page > 1) {
+      setPage(page - 1);
+    }
+  };
+
+  const handleNext = () => {
+    setPage(page + 1);
+  };
+
   return (
-    <div className="flex justify-center my-4">
+    <div className="flex justify-center items-center my-4">
       <button
-        onClick={() => setPage(page - 1)}
+        onClick={handlePrevious}
         disabled={page === 1}
-        className="px-4 py-2 m-2 bg-blue-500 text-white rounded"
+        className={`flex items-center justify-center w-12 h-12 m-2 rounded-full shadow-md text-lg transition-all ${
+          page === 1
+            ? "bg-gray-300 text-gray-600 cursor-not-allowed"
+            : "bg-primary text-white hover:bg-primary-dark hover:shadow-lg"
+        }`}
       >
-        Previous
+        <CgArrowLeft />
       </button>
-      <span className="px-4 py-2 m-2">{page}</span>
+      <span className="flex items-center justify-center text-lg font-semibold px-4 py-2 mx-3 border rounded-lg">
+        {page}
+      </span>
       <button
-        onClick={() => setPage(page + 1)}
-        className="px-4 py-2 m-2 bg-blue-500 text-white rounded"
+        onClick={handleNext}
+        className={`flex items-center justify-center w-12 h-12 m-2 rounded-full shadow-md text-lg transition-all ${
+          !page
+            ? "bg-gray-300 text-gray-600 cursor-not-allowed"
+            : "bg-primary text-white hover:bg-primary-dark hover:shadow-lg"
+        }`}
       >
-        Next
+        <CgArrowRight />
       </button>
     </div>
   );
